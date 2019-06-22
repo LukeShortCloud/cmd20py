9208b0db20845154d373d7bbb78299413708e38d
m33k <11597133+m33k@users.noreply.github.com>
2019-06-22 07:21:02 +0000

    ADDED: basic random number generator

--- cmd20.py
+++ cmd20.py
@@ -2,3 +2,12 @@
 # Name: cmd20py
 # Authors: Luke Short | Joe R
 # Repo: https://github.com/ekultails/cmd20py/
+
+import random
+
+# Number Generator
+random.seed(None)
+die1 = random.randrange(0,7)
+die2 = random.randrange(0,7)
+die_results_msg = "You rolled a:"
+print(die_results_msg,die1,die2)
