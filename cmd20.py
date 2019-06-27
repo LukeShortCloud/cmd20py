1a08f87b69c9406983b76bf636acfd5eba54c1ce
m33k <11597133+m33k@users.noreply.github.com>
2019-06-27 04:18:44 +0000

    REFACTORED: number_generator with better seed

--- cmd20.py
+++ cmd20.py
@@ -5,9 +5,24 @@
 
 import secrets
 import string
+import random
 
 def seed_generator():
     """ Function for planting seeds """
 
     ascii_char = 'string.ascii_letters + string.ascii_digits'
     seed = ''.join(secrets.choice(ascii_char) for char in range(37))
+    return seed
+
+seed = seed_generator()
+
+def random_generator():
+    """ Number generator which uses seed_generator for extra randomness """
+
+    random.seed(seed)
+    for i in range(2):
+        num_pair = random.randrange(0,7)
+        print(num_pair)
+
+
+random_generator() # Runs functions to be displayed on stdout
