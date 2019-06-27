e0f3df173874854bf0cd89cfda14e887d63d1e35
m33k <11597133+m33k@users.noreply.github.com>
2019-06-27 02:03:03 +0000

    ADDED: seed_generator function

--- cmd20.py
+++ cmd20.py
@@ -2,11 +2,12 @@
 # Name: cmd20py
 # Repo: https://github.com/ekultails/cmd20py/
 
-import random
 
-# Number Generator
-random.seed(None)
-die1 = random.randrange(0,7)
-die2 = random.randrange(0,7)
-die_results_msg = "You rolled a:"
-print(die_results_msg,die1,die2)
+import secrets
+import string
+
+def seed_generator():
+    """ Function for planting seed """
+
+    ascii_char = 'string.ascii_letters + string.ascii_digits'
+    seed = ''.join(secrets.choice(ascii_char) for char in range(37))
